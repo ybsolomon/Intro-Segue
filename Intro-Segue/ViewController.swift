0f061422067d426c4e6715f4d0e16fe3ecab75be
Yordanos SoIomon <ybsolomon@dons.usfca.edu>
2021-02-13 12:15:04 -0800

    Initial Commit

---
+++ Intro-Segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Intro-Segue
+//
+//  Created by Yordanos on 2/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
